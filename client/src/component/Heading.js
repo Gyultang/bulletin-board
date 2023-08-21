090e6e11434f263c10c03732af0ba991def52472
gyultang <rnfl0318@naver.com>
2023-08-21 15:10:56 +0900

    auth:update google로그인 기능 추가 게시글 upload수정 필요

--- client/src/component/Heading.js
+++ client/src/component/Heading.js
@@ -8,61 +8,58 @@ import { useSelector } from "react-redux";
 import firebase from "../firebase";
 
 const Heading = () => {
-  const user = useSelector((state) => state.user);
-  const navigate = useNavigate();
-  const headStyle = {
-    color: "white",
-    textDecoration: "none",
-    marginRight: "10px",
-  };
-  const LogoutHandler = () => {
-    firebase.auth().signOut();
-    navigate("/");
-  };
-  return (
-    <Navbar bg="dark" expand="lg" variant="dark">
-      <Container>
-        <Navbar.Brand href="/">Community</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to="/" style={headStyle}>
-              home
-            </Link>
+    const user = useSelector((state) => state.user);
+    const navigate = useNavigate();
+    const headStyle = {
+        color: "white",
+        textDecoration: "none",
+        marginRight: "10px",
+    };
+    const LogoutHandler = () => {
+        firebase.auth().signOut();
+        navigate("/");
+    };
+    return (
+        <Navbar bg="dark" expand="lg" variant="dark">
+            <Container>
+                <Navbar.Brand href="/">Community</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link to="/" style={headStyle}>
+                            list
+                        </Link>
 
-            <Link to="/upload" style={headStyle}>
-              upload
-            </Link>
+                        <Link to="/upload" style={headStyle}>
+                            upload
+                        </Link>
 
-            <Link to="/" style={headStyle}>
+                        {/* <Link to="/" style={headStyle}>
               list
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-        <Navbar.Collapse className="justify-content-end">
-          {user.accessToken === "" ? (
-            <Link
-              to="/login"
-              style={{ color: "white", textDecoration: "none" }}
-            >
-              login
-            </Link>
-          ) : (
-            <Navbar.Text
-              style={{
-                color: "white",
-                textDecoration: "none",
-                cursor: "pointer",
-              }}
-              onClick={(e) => LogoutHandler(e)}
-            >
-              Logout
-            </Navbar.Text>
-          )}
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+            </Link> */}
+                    </Nav>
+                </Navbar.Collapse>
+                <Navbar.Collapse className="justify-content-end">
+                    {user.accessToken === "" ? (
+                        <Link to="/login" style={{ color: "white", textDecoration: "none" }}>
+                            login
+                        </Link>
+                    ) : (
+                        <Navbar.Text
+                            style={{
+                                color: "white",
+                                textDecoration: "none",
+                                cursor: "pointer",
+                            }}
+                            onClick={(e) => LogoutHandler(e)}
+                        >
+                            Logout
+                        </Navbar.Text>
+                    )}
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 };
 
 export default Heading;
--- server/Router/post.js
+++ server/Router/post.js
@@ -7,123 +7,118 @@ const { Counter } = require("../Model/Counter.js");
 const multer = require("multer");
 const { User } = require("../Model/User");
 router.post("/submit", (req, res) => {
-  let temp = {
-    title: req.body.title,
-    content: req.body.content,
-    image: req.body.image,
-  };
-  Counter.findOne({ name: "counter" })
-    .exec()
-    .then((counter) => {
-      temp.postNum = counter.postNum;
-      User.findOne({ uid: req.body.uid })
+    let temp = {
+        title: req.body.title,
+        content: req.body.content,
+        image: req.body.image,
+    };
+    Counter.findOne({ name: "counter" })
         .exec()
-        .then((userInfo) => {
-          temp.author = userInfo._id;
-          const CommunityPost = new Post(temp);
-          CommunityPost.save().then(() => {
-            Counter.updateOne(
-              { name: "counter" },
-              { $inc: { postNum: 1 } }
-            ).then(() => {
-              res.status(200).json({ success: true });
-            });
-          });
+        .then((counter) => {
+            temp.postNum = counter.postNum;
+            User.findOne({ uid: req.body.uid })
+                .exec()
+                .then((userInfo) => {
+                    temp.author = userInfo._id;
+                    console.log("userInfo._id", userInfo._id);
+                    const CommunityPost = new Post(temp);
+                    CommunityPost.save().then(() => {
+                        Counter.updateOne({ name: "counter" }, { $inc: { postNum: 1 } }).then(() => {
+                            res.status(200).json({ success: true });
+                        });
+                    });
+                });
+        })
+        .catch((err) => {
+            res.status(400).json({ success: false });
         });
-    })
-    .catch((err) => {
-      res.status(400).json({ success: false });
-    });
 });
 
 router.post("/list", (req, res) => {
-  let sort = {};
+    let sort = {};
 
-  if (req.body.sort === "최신순") {
-    sort.createdAt = -1;
-  } else {
-    // 댓글순
-    sort.repleNum = -1;
-  }
+    if (req.body.sort === "최신순") {
+        sort.createdAt = -1;
+    } else {
+        // 댓글순
+        sort.repleNum = -1;
+    }
 
-  Post.find({
-    $or: [
-      { title: { $regex: req.body.search } },
-      { content: { $regex: req.body.search } },
-    ],
-  })
-    .populate("author")
-    .sort(sort)
-    .skip(req.body.skip) // 0, 5
-    .limit(5) // 한번에 찾을 doc 숫자
-    .exec()
-    .then((doc) => {
-      res.status(200).json({ success: true, postList: doc });
+    Post.find({
+        $or: [{ title: { $regex: req.body.search } }, { content: { $regex: req.body.search } }],
     })
-    .catch((err) => {
-      res.status(400).json({ success: false });
-    });
+        .populate("author")
+        .sort(sort)
+        .skip(req.body.skip) // 0, 5
+        .limit(5) // 한번에 찾을 doc 숫자
+        .exec()
+        .then((doc) => {
+            res.status(200).json({ success: true, postList: doc });
+        })
+        .catch((err) => {
+            res.status(400).json({ success: false });
+        });
 });
 
 router.post("/detail", (req, res) => {
-  Post.findOne({ postNum: Number(req.body.postNum) })
-    .populate("author")
-    .exec()
-    .then((doc) => {
-      console.log(doc);
-      res.status(200).json({ success: true, post: doc });
-    })
-    .catch((err) => {
-      res.status(400).json({ success: false });
-    });
+    Post.findOne({ postNum: Number(req.body.postNum) })
+        .populate("author")
+        .exec()
+        .then((doc) => {
+            console.log(doc);
+            res.status(200).json({ success: true, post: doc });
+        })
+        .catch((err) => {
+            res.status(400).json({ success: false });
+        });
 });
 // 글수정 규칙
 router.post("/edit", (req, res) => {
-  let temp = {
-    title: req.body.title,
-    content: req.body.content,
-    image: req.body.image,
-  };
-  Post.updateOne({ postNum: Number(req.body.postNum) }, { $set: temp })
-    .exec()
-    .then(() => {
-      res.status(200).json({ success: true });
-    })
-    .catch((err) => {
-      res.status(400).json({ success: false });
-    });
+    let temp = {
+        title: req.body.title,
+        content: req.body.content,
+        image: req.body.image,
+    };
+    Post.updateOne({ postNum: Number(req.body.postNum) }, { $set: temp })
+        .exec()
+        .then(() => {
+            res.status(200).json({ success: true });
+        })
+        .catch((err) => {
+            res.status(400).json({ success: false });
+        });
 });
 router.post("/delete", (req, res) => {
-  Post.deleteOne({ postNum: Number(req.body.postNum) })
-    .exec()
-    .then(() => {
-      res.status(200).json({ success: true });
-    })
-    .catch((err) => {
-      res.status(400).json({ success: false });
-    });
+    Post.deleteOne({ postNum: Number(req.body.postNum) })
+        .exec()
+        .then(() => {
+            res.status(200).json({ success: true });
+        })
+        .catch((err) => {
+            res.status(400).json({ success: false });
+        });
 });
 
 // diskStorage: multer로부터 전달받은 파일을 우리 디스크에 저장
 // destination: 어떤 경로로 저장할지
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "image/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
+    destination: function (req, file, cb) {
+        cb(null, "image/");
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
+    },
 });
 const upload = multer({ storage: storage }).single("file");
 
 router.post("/image/upload", (req, res) => {
-  upload(req, res, (err) => {
-    if (err) {
-      res.status(400).json({ success: false });
-    } else {
-      res.status(200).json({ success: true, filePath: res.req.file.path });
-    }
-  });
+    upload(req, res, (err) => {
+        if (err) {
+            res.status(400).json({ success: false });
+        } else {
+            res.status(200).json({ success: true, filePath: res.req.file.path });
+        }
+    });
 });
 
 module.exports = router;
